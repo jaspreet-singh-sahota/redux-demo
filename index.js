be9818c9bc7d58f8c109d485c3b819517bbd3c31
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-08 18:54:03 +0530

    Complete redux function

--- index.js
+++ index.js
@@ -20,10 +20,18 @@ const reducer = (state = initialState, action) => {
     case BUY_CAKE:
       return {
         ...state,
-        numberOfCakes: numberOfCakes - 1
+        numberOfCakes: state.numberOfCakes - 1
       }
     default: return state
   }
 }
 
 const store = createStore(reducer)
+console.log('initial state', store.getState())
+const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()))
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+unsubscribe()
