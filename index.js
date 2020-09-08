109c8bd4edc8e9b89450bb7b66767494e9ab75eb
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-08 19:27:40 +0530

    Add ice-creme to the store

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const redux = require('redux')
 const createStore = redux.createStore
 
 const BUY_CAKE = "BUY_CAKE"
+const BUY_ICE_CREME = "BUY_ICE_CREME"
 
 function buyCake () {
   return {
@@ -11,13 +12,35 @@ function buyCake () {
   }
 }
 
-const initialState = {
+function buyIceCreme () {
+  return {
+    type: BUY_ICE_CREME,
+    payload: 'First redux action'
+  }
+}
+
+const initialCakeState = {
   numberOfCakes: 10
 }
 
-const reducer = (state = initialState, action) => {
+const initialIceCremeState = {
+  numberOfIceCreme: 20
+}
+
+const reducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
+      return {
+        ...state,
+        numberOfCakes: state.numberOfCakes - 1
+      }
+    default: return state
+  }
+}
+
+const reducer = (state = initialIceCremeState, action) => {
+  switch (action.type) {
+    case BUY_ICE_CREME:
       return {
         ...state,
         numberOfCakes: state.numberOfCakes - 1
